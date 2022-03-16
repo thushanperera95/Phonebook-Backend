da50b1ac37691ebe0fc546130f728ed78dfd787f
Thushan <thushan.perera95@gmail.com>
2022-03-16 15:22:38 +0800

    Installed few packages for deployment to heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan("tiny", {
     skip: function (req) {
@@ -118,7 +120,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
